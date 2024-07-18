be01b27e452665731793f5858d01183c0a51659c
dhanuprys <dhanuprys@gmail.com>
2024-07-18 20:07:27 +0800

    Setup applicaton local database

--- app/src/main/java/com/dedan/mantramsesontengan/data/db/MantramDatabase.kt
+++ app/src/main/java/com/dedan/mantramsesontengan/data/db/MantramDatabase.kt
@@ -1,4 +1,32 @@
 package com.dedan.mantramsesontengan.data.db
 
-class MantramDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.dedan.mantramsesontengan.model.SavedMantram
+
+@Database(entities = [SavedMantram::class], version = 1, exportSchema = false)
+abstract class MantramDatabase : RoomDatabase() {
+    abstract fun savedMantramDao(): SavedMantramDao
+
+    companion object {
+        const val DATABASE_NAME = "mantram_database"
+
+        @Volatile
+        private var Instance: MantramDatabase? = null
+
+        fun getDatabase(context: Context): MantramDatabase {
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context,
+                    MantramDatabase::class.java,
+                    DATABASE_NAME
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+                    .also { Instance = it }
+            }
+        }
+    }
 }
--- app/src/main/java/com/dedan/mantramsesontengan/data/db/SavedMantramDao.kt
+++ app/src/main/java/com/dedan/mantramsesontengan/data/db/SavedMantramDao.kt
@@ -1,9 +1,23 @@
 package com.dedan.mantramsesontengan.data.db
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Query
+import androidx.room.Update
+import com.dedan.mantramsesontengan.model.SavedMantram
+import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface MantramDao {
-    @Query("SELECT * FROM mantram")
+interface SavedMantramDao {
+    @Query("SELECT * FROM saved_mantram")
+    fun getAllSavedMantrams(): Flow<List<SavedMantram>>
+
+    @Query("SELECT * FROM saved_mantram WHERE id = :id")
+    fun getMantramById(id: Int): Flow<SavedMantram>
+
+    @Delete
+    fun deleteMantram(mantram: SavedMantram)
+
+    @Update
+    fun updateMantram(mantram: SavedMantram)
 }
