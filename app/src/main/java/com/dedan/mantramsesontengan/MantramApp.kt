8dc7c086a34f0c50fa1fcdad487d936d906b8ab3
dhanuprys <dhanuprys@gmail.com>
2024-07-20 12:42:42 +0800

    Adding drawer item

--- app/src/main/java/com/dedan/mantramsesontengan/MantramApp.kt
+++ app/src/main/java/com/dedan/mantramsesontengan/MantramApp.kt
@@ -13,10 +13,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -25,6 +28,8 @@ import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -43,6 +48,8 @@ import androidx.navigation.compose.rememberNavController
 import com.dedan.mantramsesontengan.ui.navigation.AudioPlayerUiState
 import com.dedan.mantramsesontengan.ui.navigation.GlobalViewModel
 import com.dedan.mantramsesontengan.ui.navigation.MantramNavHost
+import com.dedan.mantramsesontengan.ui.screen.mantramselectbase.MantramSelectBaseDestination
+import com.dedan.mantramsesontengan.ui.screen.savedmantram.SavedMantramDestination
 import com.dedan.mantramsesontengan.ui.theme.MantramSesontenganTheme
 import kotlinx.coroutines.launch
 
@@ -58,7 +65,17 @@ fun MantramApp(
         drawerState = drawerState,
         gesturesEnabled = drawerState.isOpen,
         drawerContent = {
-            MantramAppDrawer()
+            MantramAppDrawer(
+                redirectTo = { route, autoClose ->
+                    navController.navigate(route)
+
+                    if (autoClose == true) {
+                        coroutineScope.launch {
+                            drawerState.close()
+                        }
+                    }
+                },
+            )
         }
     ) {
         MantramNavHost(
@@ -192,9 +209,44 @@ fun AudioBottomBar(
 }
 
 @Composable
-fun MantramAppDrawer() {
-    ModalDrawerSheet {
-        Text("Hello")
+fun MantramAppDrawer(
+    redirectTo: (String, Boolean) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    ModalDrawerSheet(modifier = modifier) {
+        NavigationDrawerItem(
+            icon = {
+                Icon(imageVector = Icons.Filled.Home, contentDescription = null)
+            },
+            label = {
+                Text("Beranda")
+            },
+            selected = true,
+            onClick = { redirectTo(MantramSelectBaseDestination.route, true) },
+            modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+        )
+        NavigationDrawerItem(
+            icon = {
+                Icon(painter = painterResource(id = R.drawable.ic_bookmark), contentDescription = null)
+            },
+            label = {
+                Text("Mantram Tersimpan")
+            },
+            selected = false,
+            onClick = { redirectTo(SavedMantramDestination.route, true) },
+            modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+        )
+        NavigationDrawerItem(
+            icon = {
+                Icon(imageVector = Icons.Filled.Info, contentDescription = null)
+            },
+            label = {
+                Text("Info Aplikasi")
+            },
+            selected = false,
+            onClick = { redirectTo(SavedMantramDestination.route, true) },
+            modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+        )
     }
 }
 
@@ -205,7 +257,7 @@ fun MantramAppBarPreview() {
     MantramSesontenganTheme {
         ModalNavigationDrawer(
             drawerContent = {
-                MantramAppDrawer()
+                MantramAppDrawer(redirectTo = { _, _ -> })
             }
         ) {
             Scaffold(
